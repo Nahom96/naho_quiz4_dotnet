04433cc520d273de121907128fa5c709cb8db9e8
Nahom Araya <naraya2@my.centennialcollege.ca>
2025-04-07 12:15:43 -0400

    Added database access message to web app

---
+++ Controllers/DatabaseAccess.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class DatabaseAccess
+{
+    public static void Main()
+    {
+        Console.WriteLine("Database access has already been done on April 1, 2025 by Nahom.");
+    }
+}
